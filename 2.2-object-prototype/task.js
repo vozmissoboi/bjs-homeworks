3df689bf06e8fd1d2a78d841127b6b43a417a7c2
Nataliia Kashyna <kashina1282@gmail.com>
2020-11-30 12:44:13 +0200

    all tasks from 2.2-object-prototype are solved

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,42 @@
-//String.prototype.isPalindrome - для задачи №1
+'use strict';
 
-function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+// Задача № 1
+
+String.prototype.isPalindrome = function isPalindrome() {
+    const string = this.toLowerCase().split(" ").join("");
+    const reversedString = this.toLowerCase().split("").reverse().join("").replace(/\s+/g, "");
+
+    if (string === reversedString) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
+// Задача № 2
+
+function getAverageMark(marks) {
+    let averageMark = 0;
+
+    if (marks.length === 0) {
+        return averageMark;
+    }
+
+    for (let i = 0; i < marks.length; i++) {
+        averageMark += marks[i];
+    }
+
+    const roundedAverage = Math.round(averageMark / marks.length);
+    return roundedAverage;
+}
+
+// Задача № 3
+
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    const now = new Date();
+    birthday = new Date(birthday).getTime();
+    const diff = now - birthday;
+    const age = Math.round(diff / (24 * 3600 * 365.25 * 1000));
+    const verdict = (age >= 18) ? true : false;
+    return verdict;
 }
